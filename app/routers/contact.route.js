fb6adf1985e199a2b0e23c806a88c853cc902522
Triết Nguyễn Minh <trietnguyen020@gmail.com>
2023-02-18 08:41:58 +0700

    Dinh nghia cac route cho tai nguyen contact

---
+++ app/routers/contact.route.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const contacts=require("../controller/contact.controller");
+const router=express.Router();
+
+router.route("/")
+    .post(contacts.create)
+    .get(contacts.findAll)
+    .delete(contacts.deleteAll);
+router.route("/favorite")
+    .get(contacts.findAllFavorite);
+router.route("/:id")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.delete);
+
+
+module.exports=router;
