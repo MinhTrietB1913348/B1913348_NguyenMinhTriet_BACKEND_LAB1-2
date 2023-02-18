4c5d979151b394d6beb6a8eeeafdf34576eef3ec
Triết Nguyễn Minh <trietnguyen020@gmail.com>
2023-02-18 08:37:11 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app.js
@@ -0,0 +1,36 @@
+const express =require("express");
+const cors= require("cors");
+const contactsRouter=require("./app/routers/contact.route");
+
+
+
+const ApiError =require("./app/api-error");
+
+const app=express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/contacts",contactsRouter);
+
+//handle 404 response
+
+app.use((req, res, next)=>{
+    //code ở đây sẽ chạy khi không có route được định nghĩa nào
+    //Khớp với yêu cầu. Gọi next() để chuyển sang middlware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Srever Error",
+    });
+});
+
+
+app.get("/",(req,res)=>{
+    res.json({message: "Welcome to contact book application."});
+});
+
+module.exports=app;
